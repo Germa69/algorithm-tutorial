da634984ed09cf79e973881545f49b730afbc158
Nguyen Duc Anh <sin2k.dev@gmail.com>
2022-03-23 23:35:13 +0700

    reverse-string

---
+++ thinking-code/reverse-string.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+    let result = [];
+
+    for (let i  = str.length - 1; i >= 0; i--) {
+        result.push(str[i]);
+    }
+
+    return result.join('');
+}
+
+const result = reverseString('Hello');
+console.log(result);
